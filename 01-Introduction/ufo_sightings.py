5a381ab60a278e6265e5983417bcc455ecd8781a
joseph misiti <joseph.misiti@gmail.com>
2014-03-04 01:16:52 -0500

    more updates

--- 01-Introduction/ufo_sightings.py
+++ 01-Introduction/ufo_sightings.py
@@ -14,5 +14,28 @@ names =["DateOccurred", "DateReported","Location", "ShortDescription","Duration"
 ufo = pd.read_csv(os.path.join("data", "ufo", "ufo_awesome.tsv"), sep="\t",names=names)
 
 # Inspect the data frame
-print(ufo.head())
-print(ufo.describe())
+#print(ufo.head())
+#print(ufo.describe())
+
+# To work with the dates, we will need to convert the YYYYMMDD string to an R Date
+# type using the 'strptime' function
+
+# But, something has gone wrong with the data. For now, we'll just ignore the errata
+# by removing those entries that have not parsed correctly.  We know that the date 
+# strings are always 8 characters long, and any deviation from this would indicate
+# a row to ignore.  We will use the 'ifelse' function to construct a vector of
+# Booleans indicating the problem rows
+ufo['DateOccurred_LENGTH'] =  ufo['DateOccurred'].apply(lambda x : len(str(x)) )
+ufo['DateReported_LENGTH'] =  ufo['DateReported'].apply(lambda x : len(str(x)) )
+
+x =  ufo['DateOccurred_LENGTH'] != 8
+y =  ufo['DateReported_LENGTH'] != 8
+good_rows = x | y
+ufo = ufo[good_rows]
+
+# # Now we can convert the strings to Date objects and work with them properly
+# pd.to_datetime(ufo['DateOccurred'])
+# pd.to_datetime(ufo['DateReported'])
+# 
+# 
+# print ufo['DateReported']
